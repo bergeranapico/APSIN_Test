8d6e96831232c1c70ca7a479b711ccc31a1ae5ab
Nicola Berger <berger@anapico.com>
2021-10-27 14:41:45 +0200

    calc:mark:xxx Befehle ergänzt, vorher wurden fälschlicherweise calc:delt:xxx Befehle genutzt

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from time import sleep
 import pyvisa as visa
 
 rm = visa.ResourceManager()
@@ -5,13 +6,14 @@ rm = visa.ResourceManager()
 
 # Verbindung zu Speki und DUT erstellen
 speki = rm.open_resource('GPIB1::20::INSTR')
-ip = '192.168.1.100'
+ip = '192.168.1.100'  # ip-adresse vom DUT
 dut = rm.open_resource('TCPIP0::' + ip + '::inst0::INSTR')
 
 # Geräte initialisieren
 speki.read_termination = '\n'
 speki.write_termination = '\n'
 speki.write('*CLS') # clear error queue
+speki.write('calc:mark:aoff')  # alle Marker entfernen
 err_speki = speki.query('syst:err?')
 print('Speki error: ' + err_speki)
 print('\n')
@@ -21,7 +23,7 @@ dut.read_termination = '\n'
 dut.write_termination = '\n'
 dut.write('*CLS')  # clear the error queue
 dut.write('syst:pres')  # set to the default settings
-dut.query('*OPC?')  # wait for commands before to be completed
+dut.query('*OPC?')  # wait for commands to be completed
 dut.write('POW:MODE CW')  # select the power mode CW
 err_dut = dut.query('syst:err?')
 dut.query('*OPC?')
@@ -29,21 +31,26 @@ print('DUT error: ' + err_dut)
 print('\n')
 
 # Test 1: RF OUT
-testf = 100e6  # Testfrequenz setzen
+testf = 250e6  # Testfrequenz definieren
+
 speki.write('disp:trac1:y:rlev 20dBm')  # Powerlevel am Speki einstellen
-speki.write('freq:cent' + str(testf))
-speki.write('freq:span' + str(2 * testf))
+speki.write('freq:cent ' + str(testf))
+speki.write('freq:span ' + str(2 * testf))
 
 
 dut.write('POW 0')  # Poweroutput vom DUT einstellen
-dut.write('freq' + str(testf))
+dut.write('freq ' + str(testf))
 dut.write('OUTP ON')  # Output am DUT aktivieren
+sleep(0.1)
 
-speki.write('calc:delt1:max')  # Marker auf den peak setzen
-value = speki.query('calc:delt1:x?')  # Frequenz vom Marker abfragen
-print(value)
+speki.write('calc:mark1:on')
+speki.write('calc:mark1:max')  # Marker auf den peak setzen
+value1 = speki.query('calc:mark1:x?')  # Frequenz vom Marker abfragen
+sleep(0.1)
+value2 = (float(value1)-testf)/testf
+print(value1)
+print(value2)
 
-speki.write('calc:delt:a0ff')
 
 # Test 2:
 
